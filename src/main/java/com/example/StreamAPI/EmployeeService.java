52b40dfbe1f86cc2752db338f085faefedd80ea4
Свинухов константин <lopata216@yandex.ru>
2023-11-13 22:16:13 +0300

    Stream API

--- src/main/java/com/example/StreamAPI/EmployeeService.java
+++ src/main/java/com/example/StreamAPI/EmployeeService.java
@@ -6,5 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface EmployeeService {
+    String  add(String firstName, String lastName,Integer dep,Integer salary);
+    String  remove(String firstName,String lastName,Integer dep,Integer salary);
+    String  search(String firstName,String lastName,Integer dep,Integer salary);
+    String max(Integer dep);
+    String  min(Integer dep);
+    Object[]  alldep(Integer dep);
+    Object[] list();
 
 }
